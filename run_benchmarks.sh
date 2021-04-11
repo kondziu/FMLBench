3ae714ebbfb677e55573aafe995cac0f4b736eba
Konrad Siek <konrad.siek@gmail.com>
2021-04-11 11:31:49 +0200

    Checking correctness.

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -8,25 +8,34 @@ function run_and_record_time {
     local command="$1"
     local benchmark="$2"
     local iteration="$3"
-    local output_file="$OUTPUT_DIR/$(basename $benchmark .fml).out"
+    local result=
 
     mkdir -p "$OUTPUT_DIR"
-    echo "STARTING [$iteration] \"$command\" run \"$benchmark\"" | tee -a "$output_file"
+    local output_file=$(mktemp "$OUTPUT_DIR/$(basename $benchmark .fml).XXX.out")
+    
+    echo "STARTING [$iteration] \"$command\" run \"$benchmark\" > $output_file"
     
     start_time_in_nanos=$(date +%s%N)
     
     "$command" run "$benchmark" >> "$output_file"
+
+    diff <(grep -e '// >' < "$benchmark" | sed 's/\/\/ > \?//') "$output_file" > "$output_file.diff"
+
+    if [ "$?" -eq 0 ]
+    then result="true"
+    else result="false"
+    fi
     
     end_time_in_nanos=$(date +%s%N)
     elapsed_time_in_nanos=$(($end_time_in_nanos - $start_time_in_nanos))
     elapsed_time_in_millis=$(($elapsed_time_in_nanos/1000/1000))
 
-    echo "$command, $(basename $benchmark .fml), $iteration, $elapsed_time_in_millis" >> "$TIMING_LOG"
+    echo "$command, $(basename $benchmark .fml), $iteration, $elapsed_time_in_millis, $result, $output_file" >> "$TIMING_LOG"
 }
 
 if [ ! -e "$TIMING_LOG" ]
 then
-    echo "FML implementation, benchmark, iteration, millis" > "$TIMING_LOG"
+    echo "FML implementation, benchmark, iteration, millis, correct, ouput file" > "$TIMING_LOG"
 fi
 
 for benchmark in benchmarks/*.fml 
